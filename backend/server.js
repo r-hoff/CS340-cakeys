3a2f7286ac06e4ea9da18f61c130fa04b46514d5
Ryan Hoff <ryanchoff@gmail.com>
2021-12-02 23:54:54 -0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
