58c4e0c3f5a6847bab2c9b3eb2bfdba7ea6021c8
Ryan Hoff <ryanchoff@gmail.com>
2021-12-02 21:22:10 -0800

    change cors options

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-let corsOptions = {
-  origin: '*',
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
