267d08f70bb64ea2ef82cbe0df984124223e447d
Ryan Hoff <ryanchoff@gmail.com>
2021-12-05 16:59:17 -0800

    comments on sql files

--- backend/SQL/data_definition_samples.sql
+++ backend/SQL/data_definition_samples.sql
@@ -8,12 +8,15 @@ DROP TABLE IF EXISTS `CustomerDiscount`;
 #############################
 # SET UP TABLES IN DATABASE #
 #############################
+
+# create table for CustomerDiscount entity
 CREATE TABLE `CustomerDiscount` (
     `discount_ID` int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     `discount_name` varchar(255) NOT NULL,
     `discount_rate` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+# create table for Customers entity
 CREATE TABLE `Customers` (
     `customer_ID` int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     `customer_first_name` varchar(255) NOT NULL,
@@ -29,6 +32,7 @@ CREATE TABLE `Customers` (
     FOREIGN KEY (`discount_ID`) REFERENCES `CustomerDiscount` (`discount_ID`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+# create table for OrderReviews entity
 CREATE TABLE `OrderReviews` (
     `review_ID` int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     `overall_rating` int(11) NOT NULL,
@@ -37,6 +41,7 @@ CREATE TABLE `OrderReviews` (
     `comment` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+# create table for Orders entity
 CREATE TABLE `Orders` (
     `order_ID` int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     `order_total_USD` float UNSIGNED NOT NULL,
@@ -51,6 +56,7 @@ CREATE TABLE `Orders` (
     FOREIGN KEY (`review_ID`) REFERENCES `OrderReviews` (`review_ID`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+# create table for Cakes entity
 CREATE TABLE `Cakes` (
     `cake_ID` int(11) NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     `cake_name` varchar(255) NOT NULL,
@@ -58,6 +64,7 @@ CREATE TABLE `Cakes` (
     `cake_retail_price_USD` float UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+# create table for OrderedCakes composite entity
 CREATE TABLE `OrderedCakes` (
     `order_ID` int(11) NOT NULL,
     `cake_ID` int(11) NOT NULL,
@@ -73,33 +80,40 @@ CREATE TABLE `OrderedCakes` (
 #############################
 # POPULATE TABLES WITH DATA #
 #############################
+
+# insert dummy data into CustomerDiscount table
 INSERT INTO `CustomerDiscount`(`discount_name`, `discount_rate`) VALUES
 ('Corporate Partnership', 0.15),
 ('Fall 2021 Sale', 0.05),
 ('Frequent Buyer', 0.1),
 ('Employee Discount', 0.25);
 
+# insert dummy data into Customers table
 INSERT INTO `Customers` (`customer_first_name`,`customer_last_name`,`customer_DOB`,`customer_street_address`,`customer_city`,`customer_state`, `customer_zip`, `customer_phone`, `customer_email`, `discount_ID`) VALUES
 ('John', 'Smith', '1996-10-10', '123 SW Street', 'Corvallis', 'OR', '97300', '123-456-7890', 'smithj@outlook.com', NULL),
 ('James', 'Smith', '1923-05-04', '456 SW Street', 'Corvallis', 'OR', '97301', '541-456-0789', 'james05@outlook.com', 1),
 ('Betty', 'Baker', '1990-01-05', '1234 Baguette Court', 'Corvallis', 'OR', '97301', '541-456-7890', 'betty_the_baker@cakeys.com', 4),
 ('Steve', 'Chowder', '2004-04-15', '832 Main Street', 'Corvallis', 'OR', '97301', '555-111-2222', 'stevie_lul@gmail.com', 3);
 
+# insert dummy data into Cakes table
 INSERT INTO `Cakes` (`cake_name`, `cake_size`, `cake_retail_price_USD`) VALUES
 ('Chocolate Strawberry Cake', 15, 45.99),
 ('Red Velvet Cake', 17, 52.99),
 ('Tiramisu Cake', 15, 43.99);
 
+# insert dummy data into OrderReviews table
 INSERT INTO `OrderReviews` (`overall_rating`, `product_quality_rating`, `service_rating`, `comment`) VALUES
 (4, 5, 5, 'My standards for cake are now way too high. Thanks Cakeys.'),
 (5, 5, 5, 'The best experience of my life, hands down.'),
 (5, 5, 5, 'The cake tasted delicious!');
 
+# insert dummy data into Orders table
 INSERT INTO `Orders` (`order_total_USD`, `order_date_time`, `credit_card_number`, `credit_card_expiration`, `order_fill_date`, `order_pickup_date`, `customer_ID`, `review_ID`) VALUES
 (88.21, '2021-10-23 14:30:27','1234567812345678','2024-11','2021-10-24','2021-10-26', 1, 1),
 (90.08, '2021-10-21 22:00:11','4321876543218765','2023-05','2021-10-11', NULL, 2, 2),
 (32.99, '2021-11-10 12:30:14','1111222233334444','2022-07', NULL, NULL, 3, NULL);
 
+# insert dummy data into OrderedCakes table
 INSERT INTO `OrderedCakes` (`order_ID`, `cake_ID`, `cake_sale_price_USD`, `cake_qty`, `order_status`) VALUES
 (1, 1, 45.99, 2, 'Completed'),
 (2, 2, 45.04, 1, 'Awaiting Pickup'),
--- backend/SQL/data_manipulation_queries.sql
+++ backend/SQL/data_manipulation_queries.sql
@@ -1,6 +1,8 @@
 ##################
 # SELECT QUERIES #
 ##################
+
+# select statements for all entities
 SELECT * FROM CustomerDiscount;
 SELECT * FROM Customers;
 SELECT * FROM OrderReviews;
@@ -8,41 +10,53 @@ SELECT * FROM Orders;
 SELECT * FROM Cakes;
 SELECT * FROM OrderedCakes;
 
-# SELECT FOR SEARCH FUNCTION
+# select statement for search function (search Customers by last name)
 SELECT * FROM Customers
 WHERE customer_first_name LIKE '%:customer_first_name_input%' OR
 customer_last_name = '%:customer_last_name_input%';
 
+
 ##################
 # INSERT QUERIES #
 ##################
+
+# insert new record into CustomerDiscount
 INSERT INTO `CustomerDiscount`(`discount_name`, `discount_rate`) VALUES
 (:discount_name_input, discount_rate_input);
 
+# insert new record into Customers
 INSERT INTO `Customers` (`customer_first_name`,`customer_last_name`,`customer_DOB`,`customer_street_address`,`customer_city`,`customer_state`, `customer_zip`, `customer_phone`, `customer_email`, `discount_ID`) VALUES
 (:customer_first_name_input, :customer_last_name_input, :customer_DOB_input, :customer_street_address_input, :customer_city_input, :customer_state_input, :customer_zip_input, :customer_phone_input, :customer_email_input, :discount_ID_input);
 
+# insert new record into Cakes
 INSERT INTO `Cakes` (`cake_name`, `cake_size`, `cake_retail_price_USD`) VALUES
 (:cake_name_input, :cake_size_input, :cake_retail_price_USD_input);
 
+# insert new record into OrderReviews
 INSERT INTO `OrderReviews` (`overall_rating`, `product_quality_rating`, `service_rating`, `comment`) VALUES
 (:overall_rating_input, :product_quality_rating_input, :service_rating_input, :comment_input);
 
+# insert new record into Orders
 INSERT INTO `Orders` (`order_total_USD`, `order_date_time`, `credit_card_number`, `credit_card_expiration`, `order_fill_date`, `order_pickup_date`, `customer_ID`, `review_ID`) VALUES
 (:order_total_USD_input, :order_date_time_input, :credit_card_number_input, :credit_card_expiration_input, :order_fill_date_input, :order_pickup_date_input, :customer_ID_input, :review_ID_input);
 
+# insert new record into OrderedCakes
 INSERT INTO `OrderedCakes` (`order_ID`, `cake_ID`, `cake_sale_price_USD`, `cake_qty`, `order_status`) VALUES
 (:order_ID_input, :cake_ID_input, :cake_sale_price_USD_input, :cake_qty_input, :order_status_input);
+
 
 ##################
 # UPDATE QUERIES #
 ##################
+
+# update CustomerDiscount by discount_ID
 UPDATE CustomerDiscount
 SET
 `discount_name` = :discount_name_input,
 `discount_rate` = :discount_rate_input
 WHERE discount_ID = :discount_ID_input;
 
+# update Customers by customer_id
 UPDATE Customers
 SET
 `customer_first_name` = :customer_first_name_input,
@@ -57,6 +71,7 @@ SET
 `discount_ID` = :discount_ID_input
 WHERE customer_id = :customer_id_input;
 
+# update Orders by order_ID
 UPDATE Orders
 SET
 `order_total_USD` = :order_total_USD_input,
@@ -69,6 +84,7 @@ SET
 `review_ID` = :review_ID_input
 WHERE order_ID = :order_ID_input;
 
+# update OrderReviews by review_ID
 UPDATE OrderReviews 
 SET 
 `overall_rating` = :overall_rating_input,
@@ -77,6 +93,7 @@ SET
 `comment` = :comment_input
 WHERE review_ID = :review_ID_input;
 
+# update OrderedCakes by order_ID & cake_ID
 UPDATE OrderedCakes
 SET
 `order_ID` = :order_ID_input,
@@ -86,6 +103,7 @@ SET
 `order_status` = :order_status_input
 WHERE order_ID = :order_ID_input and cake_ID = :cake_ID_input;
 
+# update Cakes by cake_ID
 UPDATE Cakes 
 SET 
 `cake_name` = :cake_name_input,
@@ -93,23 +111,31 @@ SET
 `cake_retail_price_USD` = :cake_retail_price_USD_input
 WHERE cake_ID = :cake_ID_input;
 
+
 ##################
 # DELETE QUERIES #
 ##################
+
+# delete from CustomerDiscount by discount_ID
 DELETE FROM CustomerDiscount
 WHERE discount_ID = :discount_ID_input;
 
+# delete from Customers by customer_ID
 DELETE FROM Customers
 WHERE customer_ID = :customer_ID_input;
 
+# delete from Cakes by cake_ID
 DELETE FROM Cakes
 WHERE cake_ID = :cake_ID_input;
 
+# delete from Orders by order_ID
 DELETE FROM Orders
 WHERE order_ID = :order_ID_input;
 
+# delete from OrderReviews by review_ID
 DELETE FROM OrderReviews
 WHERE review_ID = :review_ID_input;
 
+# delete from OrderedCakes by order_ID & cake_ID
 DELETE FROM OrderedCakes
 WHERE order_ID = :order_ID_input AND cake_ID = :cake_ID_input;
